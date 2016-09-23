2abf5191dbc31f9e62d90ea38e35072a5dc98616
Manifestus <link_figueroa20@hotmail.com>
2016-09-23 02:42:37 -0600

    agregar mas personas

--- Main.cpp
+++ Main.cpp
@@ -88,11 +88,53 @@ int main(int argc, char const *argv[])
 
 		if (type == "admin")
 		{
-			
+			string name;
+			int edad;
+			int identidad;
+
+			cout<< "Ingrese opcion!" << endl << "1. Agregar Personas!" << endl << "2. Mesas"<< endl;
+			int opcAdminMain = 0;
+
+			cin>> opcAdminMain;
+
+			if (opcAdminMain == 1)
+			{
+				int respPersonas = 0;
+				while(respPersonas == 0)
+				{
+					cout<< "Ingrese tipo!" << endl << "1. Administrador" << endl<< "2. Repartidor" << endl << "3. Jugador";
+					int opcAdminPersona = 0;
+
+					cin>> opcAdminPersona;
+
+					cout<< "Ingrese Nombre!"<< endl;
+					cin>> nombre;
+
+					cout<< "Ingrese edad!" << endl;
+					cin>> edad;
+
+					cout<< "Ingrese ID!";
+					cin>> identidad;
+
+					if (opcAdminPersona == 1)
+					{
+						cout<< "Ingrese la experiencia laboral!" < endl;
+						int exp;
+						cin>> exp;
+
+						cout<< "Ingrese opcion!" << endl << "1. Dificil"<< endl<< "2. Intermedio" << endl << "3. Facil" << endl;
+					}
+
+					cout<< "Desea continuar creando personas!? 0/1"<< endl;
+					cin>> respPersonas;
+				}
+				
+				
+			}
 		}
 
 		cout<< "Desea continuar!? 0/1"<< endl;
-		cinn>> resp;
+		cin>> resp;
 	}
 
 	return 0;
